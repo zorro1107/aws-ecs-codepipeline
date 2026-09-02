9f25f12ba42df84abfaa985f4c7f6ee46a59965e
Anirudh Navuduri <122357648+zorro1107@users.noreply.github.com>
2026-09-02 16:46:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello from ECS Fargate! Version: 1.0\n");
+});
+
+server.listen(3000, "0.0.0.0", () => {
+    console.log("Server running on port 3000");
+});
